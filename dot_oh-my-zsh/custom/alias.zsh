63a9ca17806412682c0e151c9b85bb6192ad1dfa
alexandre.gossard <alexandre.gossard@accenture.com>
2025-11-20 14:34:33 +0100

    Add custom bin to PATH and chezmoi aliases

--- dot_oh-my-zsh/custom/alias.zsh
+++ dot_oh-my-zsh/custom/alias.zsh
@@ -1,5 +1,15 @@
+# Add custom bin to PATH
+export PATH="$HOME/.oh-my-zsh/custom/bin:$PATH"
+
 alias path="echo $PATH | sed 's/:/\n\g'"
 alias zshrc="$EDITOR ~/.zshrc; exec zsh"
 alias p='python3'
 alias l='eza -1 -F -l --git'
 alias cat='bat -p'
+
+# Chezmoi shortcuts
+alias cm='chezmoi'
+alias cme='chezmoi edit'
+alias cma='chezmoi apply'
+alias cmd='chezmoi diff'
+alias cms='chezmoi status'
